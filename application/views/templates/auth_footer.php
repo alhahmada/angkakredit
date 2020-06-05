d908cfdbc957028a8a606d02943fe30191e37d32
alhahmada <16.8993@stis.ac.id>
2020-06-05 22:21:58 +0700

    5-6-20

--- application/views/templates/auth_footer.php
+++ application/views/templates/auth_footer.php
@@ -18,66 +18,26 @@
 
 
 <!-- Footer -->
-<footer style="background-color: #282b3b; height: 5px;">
 
-</footer>
+<style>
+    .small {
+        font-size: 11px;
+        color: white;
+    }
+
+    .med {
+        font-size: 13px;
+        color: white;
+    }
+</style>
 
 
-<footer class="sticky-footer" style="background-color: #494e57;">
-    <style>
-        .text-ftr {
-            font-family: Roboto;
-            color: white;
-            width: 33.2%;
-            margin-top: 0;
-        }
 
-        .small {
-            font-size: 11px;
-            margin-bottom: 1rem;
-        }
+<footer style="height: 5rem; background-color: #282b3b;">
 
-        .med {
-            font-size: 14px;
-            /* font-weight: bold; */
-        }
-
-        .ftr-divider {
-            width: 0;
-            border-right: 2px solid #5a5c69;
-            height: 5rem;
-        }
-    </style>
-
-    <div class="" style="background-color: #494e57; height: 5rem;">
-        <div class="row" style="width: 100%; align-items: center;">
-            <div class="text-ftr text-center">
-                <h5 class="small">Contact</h5>
-                <h5 class="med">Email 16.8993@stis.ac.id</h5>
-            </div>
-            <div class="ftr-divider"></div>
-            <div class="text-ftr text-center">
-                <h5 class="small">About Developer</h5>
-                <h5 class="med">Alha Ahmada Belasae | 16.8993 | Polstat STIS</h5>
-            </div>
-            <div class="ftr-divider"></div>
-            <div class="text-ftr text-center">
-                <h5 class="small">Version</h5>
-                <h5 class="med">Versi 1.0, Last Modified June 2020</h5>
-            </div>
-        </div>
-    </div>
-
-</footer>
-
-<footer class="sticky-footer" style="height: 4rem; background-color: #282b3b;">
-
-    <div class="" style="background-color: #282b3b;">
-        <div class="container my-auto">
-            <div class="copyright text-center" style="color: white;">
-                <span>Copyright &copy; Angka Kredit Politeknik Statistika STIS 2020</span>
-            </div>
-        </div>
+    <div class="container text-center pt-4" style="background-color: #282b3b; color: white;">
+        <h5 class="small">Versi 1.0, Last Modified June 2020</h5>
+        <h5 class="med">Copyright &copy; Angka Kredit Politeknik Statistika STIS 2020</h5>
     </div>
 
 </footer>
